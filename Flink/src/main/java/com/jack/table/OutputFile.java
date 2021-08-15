74c9ad76fde8b7f85fb4a9d920683aaf63ae70b4
Your Name <you@example.com>
2021-08-15 14:25:06 +0800

    数据输出到外部文件环境

--- Flink/src/main/java/com/jack/table/OutputFile.java
+++ Flink/src/main/java/com/jack/table/OutputFile.java
@@ -6,6 +6,7 @@ import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.table.descriptors.Csv;
 import org.apache.flink.table.descriptors.FileSystem;
+import org.apache.flink.table.descriptors.Kafka;
 import org.apache.flink.table.descriptors.Schema;
 import org.apache.flink.types.Row;
 
@@ -16,6 +17,21 @@ public class OutputFile {
         env.setParallelism(1);
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
         String path = "F:\\RoadPinnacle\\Flink\\Flink\\Flink\\src\\main\\resources\\sensor.txt";
+        /**
+         连接kafka
+         tableEnv.connect(new Kafka().version("1.1")
+         .topic("sensor")
+         .property("zookeeper.connect", "localhost:2181")
+         .property("bootstrap.servers","localhost:9092"))
+         .withFormat(new Csv())
+         .withSchema(new Schema().field("id", DataTypes.STRING())
+         .field("ts", DataTypes.BIGINT())
+         .field("temp", DataTypes.DOUBLE()))
+         .createTemporaryTable("kafkaData");
+         Table kafkaData = tableEnv.from("kafkaData");
+         *
+         */
+
         // 注册表
         tableEnv.connect(new FileSystem().path(path))
                 .withFormat(new Csv())
@@ -30,9 +46,12 @@ public class OutputFile {
         Table filterResult = inputData.select("id,temp").filter("id='sensor_1'");
         tableEnv.toAppendStream(filterResult, Row.class).print("111");
 //        执行
+
+        // 连接外部文件系统
         tableEnv.connect(new FileSystem().path("F:\\RoadPinnacle\\Flink\\Flink\\Flink\\src\\main\\resources\\out.txt"))
                 .withFormat(new Csv())
-                .withSchema(new Schema().field("id", DataTypes.STRING())
+                .withSchema(new Schema()
+                        .field("id", DataTypes.STRING())
                         .field("temp", DataTypes.DOUBLE())).createTemporaryTable("output");
         tableEnv.insertInto("output", filterResult);
 
