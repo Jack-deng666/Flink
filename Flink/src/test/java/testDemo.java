7db09d9a5ba73999d4f8be13240b6b2c7b839ed6
dengjirui <1298573296@qq.com>
2021-08-18 12:46:57 +0800

    kafka数据到mysql

--- Flink/src/test/java/testDemo.java
+++ Flink/src/test/java/testDemo.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * @author jack Deng
  * @version 1.0
  * @date 2021/8/13 18:08
  */
 public class testDemo {
+    public char[] empty;
     public static void main(String[] args) {
-        Double aDouble = 10.1;
-        System.out.println(aDouble);
-        int i = aDouble.intValue();
-        System.out.println(i);
+
+        //        for (int i=0;i<256;i++)
+        //            for (int j =0;j<256;j++) {
+        //                String temp_string = String.valueOf(i) + String.valueOf(j);
+        //                System.out.println(temp_string+" "+temp_string.hashCode());
+        //          }
+        char[] temp = {'a', 'b'};
+        String str = new String(temp);
+        System.out.println(str);
+        char [] result = str.toCharArray();
+        System.out.println(result);
+
+        HashMap<String, Integer> hashmap = new HashMap<>();
+        hashmap.put("dengjirui",666);
+        Integer dengjirui = hashmap.get("dengjirui");
+        System.out.println(dengjirui);
+        System.out.println(hashmap);
+        System.out.println(hashmap.containsValue(666));
+
     }
 }
